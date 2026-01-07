8e4694704b12b6a0bf104c3f713269790d835bdb
Swapnil-CS775 <swapnilgavali1414@gmail.com>
2026-01-08 00:22:14 +0530

    Add Spring Boot backend

---
+++ algo-trading/src/main/java/com/algo/AlgoTradingApplication.java
@@ -0,0 +1,12 @@
+package com.algo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoTradingApplication.class, args);
+	}
+}
